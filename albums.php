377d1932a53c688a09c5945f150192ce9631a8a8
acmalam <acmalam@teiemt.gr>
2019-05-10 00:00:37 +0300

    starting eshop
    files changed to php
    first try eshop

---
+++ albums.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title> Placebo </title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<header>
+		
+		<div class="row">
+			
+			<div class="logo">
+				<img src= "Layer.png" > 
+			</div>
+			
+			<ul class= "main-nav">
+				<li class="active"><a href="index.php"> HOME </a> </li>
+				<li><a href="albums.php"> ALBUMS </a> </li>
+				<li><a href="tours.php"> TOURS </a> </li>
+				<li><a href="aboutus.php"> ABOUT US</a> </li>
+				<li><a href="eshop.php"> eSHOP </a> </li>
+			</ul>
+			
+		</div>
+		
+		<div class="periexomeno">
+		   
+		   <div class="gallery">
+		   <a target="_blank" href="coveralbums/album1.jpg">
+		   <img src="coveralbums/album1.jpg" alt="Loud Like Love" width="150" height="150">
+		   </a>
+		   </div>
+		   
+		   <div class="gallery">
+		   <a target="_blank" href="coveralbums/album2.jpg">
+		   <img src="coveralbums/album2.jpg" alt="Battle For The Sun" width="150" height="150">
+		   </a>
+		   </div>
+		  
+		  </div>
+		
+	</header>
+	
+</body>
+
+</html>
---
+++ eshop1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Placebo eShop </title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/eshop-style-main.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"> <!-- scalable on all devices -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> <!-- using jquery instead of javascript -->
+	<script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<!-- Top Nav Bar -->
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <a class="navbar-brand" href="index.php">Return To WebSite</a>
+		</div>
+		<ul class="nav navbar-nav"> <!-- unlisted items -->
+		  <li class="active"><a href="eshop.php">eShop</a></li><!-- listed items -->
+		  <li><a href="eshop1.php">A PLACE FOR US TO DREAM</a></li>
+		  <li><a href="eshop2.php">CLOTHING</a></li>
+		  <li><a href="#">ACCESSORIES</a></li>
+		  <li><a href="#">MEDIA</a></li>
+		</ul>
+	  </div>
+	</nav>
+
+	<!-- Header -->
+	<div id="headerWrapper">
+		<div id="logotext"></div>	
+	</div>
+
+
+	<div class="container-fluid">
+		<!-- left Sidebar -->
+		<div class="col-md-2">Left Side Bar</div>
+		
+		<!-- main content -->
+		<div class="col-md-8">
+			<div class="row">
+				<h2 class="text-center"> Feature Products</h2>
+				<div class="col-md-3">
+					<h4>A Place For Us To Dream – 20 Years Of Placebo’ Deluxe Box Set, Black Vinyl Edition</h4>
+					<img src="eshop-img/album1.png" alt= "Deluxe Box Set"  class="img-thumb"/>
+					<p class="list-price text-danger">List Price<s>$70.00</s></p>
+					<p class="price">Our Price: $35.00</p>
+					<button type="button" class="btn btn-sm bt-success" data-toggle="modal" data-targer="#details-1"> Details</button>
+				</div>
+			</div>
+		</div>
+		
+		<!-- right Sidebar -->
+		<div class="col-md-2">Right Side Bar</div>
+	</div>
+
+	<!-- javascript -->
+	<script>
+		jQuery(window).scroll(function(){
+			var vscroll = jQuery(this).scrollTop();
+			jQuery('#logotext').css({
+				"transform" : "translate(0px,"+vscroll/2+"px)"
+			})
+		});
+	</script>	
+</body>
+
+</html>
